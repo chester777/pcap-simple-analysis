33c18b5d8673895e6f1d8039333faedd46b8b094
chester777 <aster9797@gmail.com>
2019-03-20 22:03:39 +0900

    first commit

--- telegram_bot/TelegramBotWrapper.py
+++ telegram_bot/TelegramBotWrapper.py
@@ -1,70 +1,19 @@
-import os
-import logging
-
+import telegram
 from telegram.ext import Updater
-from telegram.ext import CommandHandler, MessageHandler, Filters
 
-from errors.TelegramBot import *
-
-TELEGRAM_BOT_API_TOKEN = os.getenv('TELEGRAM_BOT_API_TOKEN', None)
-
-if TELEGRAM_BOT_API_TOKEN is None:
-    raise TokenError()
-
-elif not len(TELEGRAM_BOT_API_TOKEN) > 0:
-    raise TokenError()
-
-logger = logging.getLogger('pcap-simple-analysis-logger')
-
+API_TOKEN = '892925774:AAH-w0XBPve7nIuJhVtCeSpNGyXO4NQHDyo'
 
 class TelegramBotWrapper:
 
     def __init__(self):
-        updater = Updater(token=TELEGRAM_BOT_API_TOKEN, use_context=True)
-        dispatcher = updater.dispatcher
+        self.updater = Updater(token=API_TOKEN, user_context=True)
+        self.dispatcher = updater.dispatcher
 
-        start_handler = CommandHandler('start', self.start_cmd)
-        msg_handler = MessageHandler(Filters.all, self.msg_handler)
-
-        dispatcher.add_handler(msg_handler)
-        dispatcher.add_handler(start_handler)
-        dispatcher.add_error_handler(self.error_handler)
-
-        logger.info('* Start Telegram Bot')
-
-        updater.start_polling()
-        updater.idle()
-
-    @staticmethod
-    def start_cmd(update, context):
-        context.bot.send_message(
-            chat_id=update.message.chat_id,
-            text="I'm a bot, please talk to me!"
-        )
-
-    @staticmethod
-    def msg_handler(update, context):
-        try:
-            message = 'Unknown Message'
-            if update.message.document is not None:
-                # file handling
-                file_id = update.message.document.file_id
-                file_name = update.message.document.file_name
-                new_file = context.bot.get_file(file_id)
-                new_file.download(file_name)
-
-            else:
-                # text handling
-                message = update.message.text
-
-            context.bot.send_message(
-                chat_id=update.message.chat_id,
-                text=message
-            )
-
-        except Exception as e:
-            print(e)
-
-    @staticmethod
-    def error_handler(update, context):
+    def start(self):
         pass
+        
+
+        #updates = bot.getUpdates()
+
+        #for update in updates:
+        #    print(update.message)
--- telegram_bot/test.py
+++ telegram_bot/test.py
@@ -1,12 +1,8 @@
-from telegram_bot.TelegramBotWrapper import TelegramBotWrapper
-from commons.LogWrapper import LogWrapper
-
+from TelegramBotWrapper import TelegramBotWrapper
 
 class Main:
     def __init__(self):
-        LogWrapper('pcap-simple-analysis-logger')
         TelegramBotWrapper()
-
 
 if __name__ == '__main__':
     Main()
