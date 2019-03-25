c20f41d7b222e28622af072b7bc9ff57a427da3d
chester777 <aster9797@gmail.com>
2019-03-25 22:57:32 +0900

    [190325_01_TelegramBot] * 추가 내역 - 텔래그램 봇 API 관련 랩핑 클래스 추가 * todo: Telegram Bot - pcap, 커맨드, 나머지 예외 처리 로직 추가 필요 - pcap 다운로드 관련 규칙 설정 필요 - pcap 다운로드 후 처리하는 모듈 호출방식 고민 필요 : 지금 사용하는 Telegram Bot 관련 모듈이 내부를 Thread 로 처리하여 메세지 핸들링 함수를 호출하는 형식을 가질수가 없음

--- errors/TelegramBot.py
+++ errors/TelegramBot.py
@@ -1,6 +1,4 @@
 class TokenError(Exception):
-    """
-    error : -
-    """
+
     def __str__(self):
         return "Token is not valid"
--- telegram_bot/test.py
+++ telegram_bot/test.py
@@ -1,7 +1,9 @@
-from TelegramBotWrapper import TelegramBotWrapper
+from telegram_bot.TelegramBotWrapper import TelegramBotWrapper
+from commons.LogWrapper import LogWrapper
 
 class Main:
     def __init__(self):
+        LogWrapper('pcap-simple-analysis-logger')
         TelegramBotWrapper()
 
 if __name__ == '__main__':
